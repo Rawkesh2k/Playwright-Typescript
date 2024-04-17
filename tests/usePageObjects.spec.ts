f1bfefd11650bc8e041a104c40b6f28607ba3b79
Rawkesh2k <rakeshkumarkurupati@gmail.com>
2024-04-17 18:42:14 -0400

    updating usePageObjects.spec.ts

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from "@playwright/test";
-import {NavigationPage} from "../PageObjects/navigationPage"
+import { NavigationPage } from "../PageObjects/navigationPage";
+import { faker } from "@faker-js/faker";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 });
 
-test('Navigate to the page',async ({ page }) => {
-   const  navigateTo =  new NavigationPage(page)
-   await navigateTo.formlayouts()
-  });
+test("Navigate to the page", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  await navigateTo.formlayouts();
+});
